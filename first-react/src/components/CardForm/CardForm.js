6fdd04c56ec50983fab98dd6cf9707a3baff8662
Pawel Krol <paullkroll@icloud.com>
2023-08-13 16:16:31 +0200

    Install all redux files, add initialState and store js files in redux catalog, add useSelector in List and Column

--- first-react/src/components/CardForm/CardForm.js
+++ first-react/src/components/CardForm/CardForm.js
@@ -4,10 +4,10 @@ import styles from './CardForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 
 const CardForm = props => {
-    const [title,setTitle] = useState('');
+    const [title, setTitle] = useState('');
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId, props.action);
+        props.action({ title: title }, props.columnId);
         setTitle('');
     };
 
--- first-react/src/components/Column/Column.js
+++ first-react/src/components/Column/Column.js
@@ -1,18 +1,19 @@
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 import styles from './Column.module.scss';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id))
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
             <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id}>{card.title}</Card>)}
+                {cards.map(card => <Card key={card.id}>{card.title}</Card>)}
             </ul>
-            <CardForm columnId={props.id} action={props.addCard}/>
+            <CardForm columnId={props.id} action={props.addCard} />
         </article>
-    );
-    
+    );    
 };
 
 export default Column;
